d66f39af8880110ea15595ed45a270858ef29bfa
Bharath H U <bharathhubharath@gmail.com>
2026-04-20 09:24:42 +0530

    Time: 39 ms (6.43%), Space: 22.1 MB (69.54%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,21 @@
+class Solution:
+    def createOccStr(self,s):
+        ascii = [0] * 26
+        occStr = ""
+        for i in range(0, len(s)):
+            ascii[ord(s[i])-97] += 1
+        for i in range(0, len(ascii)):
+            if ascii[i] > 0:
+                occStr += chr(i + 97)
+                occStr += chr(ascii[i] + 48)
+        return occStr
+    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict = defaultdict(list)
+        for i in range (0, len(strs)):
+            occStr = self.createOccStr(strs[i])
+            dict[occStr].append(strs[i])
+        res = []
+        for key, val in dict.items():
+            res.append(val)
+        return res
