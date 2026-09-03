275a6b78e8ab45229865eeaebf6a32a0224eda0b
Bharath H U <bharathhubharath@gmail.com>
2026-09-03 18:46:25 +0530

    Time: 4 ms (90.62%), Space: 19.4 MB (94.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,18 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newstr = ""
 
-        for i in range(len(s)):
-            if "A" <= s[i] <= "Z":
-                newstr += chr(ord(s[i]) + 32)
-            elif ("0" <= s[i] <= "9") or ("a" <= s[i] <= "z"):
-                newstr += s[i]
+        left = 0
+        right = len(s) - 1
 
-        # check palindrome
-        left, right = 0, len(newstr) - 1
         while left < right:
-            if newstr[left] != newstr[right]:
+
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
                 return False
+
             left += 1
             right -= 1
 
